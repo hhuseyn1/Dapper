9c981550759146cbe54e30af20835c3f2b621c5e
Huseyn Hamidov <88426345+hhuseyn1@users.noreply.github.com>
2023-03-10 13:43:55 +0400

    Add search event

--- Source/MainWindow.xaml.cs
+++ Source/MainWindow.xaml.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 using System.IO;
 using System.Windows;
+using System.Windows.Controls;
+using Z.BulkOperations;
 
 namespace Source;
 
@@ -28,5 +31,62 @@ public partial class MainWindow : Window
 
         connectionString = configuration.GetConnectionString("ProductDb");
         conn = new SqlConnection(connectionString);
+    }
+
+    private void CreateDbBtn_Click(object sender, RoutedEventArgs e)
+    {
+        var reader = conn.ExecuteReader("SELECT * FROM Product");
+        table = new DataTable();
+        table.Load(reader);
+        DataGrid.ItemsSource = table.AsDataView();
+    }
+
+    private void SearchBtn_Click(object sender, RoutedEventArgs e)
+    {
+        string dcommand;
+        if (string.IsNullOrWhiteSpace(Searchtxtbox.Text) || string.IsNullOrEmpty(Searchtxtbox.Text))
+        {
+            dcommand = $"SELECT * FROM Product";
+            command = new SqlCommand(dcommand, conn);
+        }
+
+        else
+        {
+            dcommand = $"SELECT * FROM Product WHERE LOWER(Name) LIKE '%'+LOWER(@a)+'%'";
+        }
+        var reader = conn.ExecuteReader(dcommand, new {a=Searchtxtbox.Text});
+        table = new DataTable();
+        table.Load(reader);
+        DataGrid.ItemsSource = table.AsDataView();
+
+        Searchtxtbox.Text = null;
+    }
+
+    private void SelectCommand(string command)
+    {
+        var reader = conn.ExecuteReader(command);
+        table = new DataTable();
+        table.Load(reader);
+        DataGrid.ItemsSource = table.AsDataView();
+    }
+
+    private void AddprBtn_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void EditprBtn_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void RemoveprBtn_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void ClearListBtn_Click(object sender, RoutedEventArgs e)
+    {
+
     }
 }
